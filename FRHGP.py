5c2c3277dcacc8eec2f402eea9221ed45876ebe0
Laguting <lagutingmariconjaneg@gmail.com>
2023-04-23 09:29:37 +0800

    Placee the initial pseudocode

---
+++ FRHGP.py
@@ -0,0 +1,11 @@
+# Laguting, Maricon Jane G.
+# BSCPE 1-4 
+# Task: Write a Python program that read a file containing the name of 20 students together with their GWA. The program will outputs the name of 
+# the student who got the highest GWA (including the GWA).
+# Welcome the user to the program
+# Introduce FRHGP
+# Open gwalist.txt
+# Read the contents
+# Get the highest GWA
+# Display the Highest GWA
+# Close the Program
