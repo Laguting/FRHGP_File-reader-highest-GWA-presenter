247ccedabd9e0bc1c81511262be8abeb2680d97e
Laguting <lagutingmariconjaneg@gmail.com>
2023-04-23 09:37:47 +0800

    modified the welcome message by adding a space and installing pyfiglet as well as the termcolor

--- FRHGP.py
+++ FRHGP.py
@@ -3,6 +3,14 @@
 # Task: Write a Python program that read a file containing the name of 20 students together with their GWA. The program will outputs the name of 
 # the student who got the highest GWA (including the GWA).
 # Welcome the user to the program
+from pyfiglet import Figlet
+from termcolor import colored
+ti_tle = Figlet(font = "starwars")
+print("\33[7m-+°\33[0m" * 45)
+print ("")
+print(colored(ti_tle.renderText("WELCOME !"), "blue"))
+print("\33[7m-+°\33[0m" * 45)
+
 # Introduce FRHGP
 # Open gwalist.txt
 # Read the contents
